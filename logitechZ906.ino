9c4997ffd56585eec6928cb55ab7327ab5fbb86b
Lelisvaldo <lelisvaldo@outlook.com>
2022-06-28 19:57:58 -0300

    Modelo Funcional

--- logitechZ906.ino
+++ logitechZ906.ino
@@ -11,6 +11,10 @@ const int relayPin =  4;                // the number of the RELAY pin
 
 // variables will change:
 int buttonState = 0;                    // variable for reading the pushbutton status
+int alexaState = 0;                    // variable for reading the alexaState status
+
+bool button;
+bool alexa;
 
 fauxmoESP fauxmo;
 
@@ -24,6 +28,7 @@ void wifi() {
 
 void setup() {
   wifi();
+  Serial.begin(115200);
   pinMode(relayPin, OUTPUT);            // initialize the relayPin pin as an output:
   pinMode(buttonPin, INPUT);            // initialize the pushbutton pin as an input:
   fauxmo.createServer(true);
@@ -35,7 +40,9 @@ void setup() {
     Serial.printf("[MAIN](%s) state: %s\n", device_name, state ? "ON" : "OFF");
     if (strcmp(device_name, logiZ906) == 0) {
       if (state) {
-        commandChange(1);
+        alexa = true;
+      } else {
+        alexa = false;
       }
     }
   });
@@ -43,18 +50,20 @@ void setup() {
 
 void loop() {
   fauxmo.handle();
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-    commandChange(1);
+  button = digitalRead(buttonPin);
+  if (buttonState != button) {
+    commandChange();
+  }
+  else if (alexaState != alexa) {
+    commandChange();
+    alexaState = !alexaState;
   }
 }
 
-void commandChange(int command) {
-  if (command == 1) {
-    digitalWrite(relayPin, HIGH);
-    fauxmo.setState(logiZ906, true, 255);
-    delay(500);
-    digitalWrite(relayPin, LOW);
-    fauxmo.setState(logiZ906, true, 0);
-  }
+void commandChange() {
+  digitalWrite(relayPin, HIGH);
+  fauxmo.setState(logiZ906, true, 255);
+  delay(500);
+  digitalWrite(relayPin, LOW);
+  fauxmo.setState(logiZ906, false, 0);
 }
